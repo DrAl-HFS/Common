a8477a24ead8eb91c9f18c14a76370e1fce9bd69
DrAl-HFS <DrAl-HFS@github.com>
2020-10-06 11:44:37 +0100

    Testing OK

--- MBD/ledMatrix.c
+++ MBD/ledMatrix.c
@@ -1,66 +1,23 @@
+// Common/MBD/ledMatrix.h - Hacks for IS31FL3731 LEDshim
+// https://github.com/DrAl-HFS/Common.git
+// Licence: GPL V3
+// (c) Project Contributors Oct. 2020
+
 #include "ledMatrix.h"
+#include "lumissil.h"
+#include "ledMapRGB.h"
 
-// IS31FL3731
 
-#define ELEMENT_COUNT (2*8*9)
-#define FLAG_BYTES (ELEMENT_COUNT>>3)
-#define PWM_BYTES ELEMENT_COUNT
-typedef struct
-{
-   U8 addr[1];//={0x00};
-   U8 enable[FLAG_BYTES];
-   //U8 addr[1];//={0x12};
-   U8 blink[FLAG_BYTES];
-   //U8 addr[1];//={0x24};
-   U8 pwm[PWM_BYTES];
-} FramePage;
-
-typedef struct
-{
-   U8 addr[1];//={0x00};
-   U8 reg[13]; // NB: 0x7 read-only
-} ControlPage;
-
-#define REG_PAGE_SEL 0xFD
-#define CTRL_PAGE 0x0B
-
-void setByMask (U8 r[], const int n, const U8 m[], const U8 v[2])
-{
-   int i, j;
-   for (i= 0; i<n; i++)
-   {
-      j= (m[i>>3] >> (i & 0x7)) & 0x01; // bit-endianess
-      r[i]= v[j];
-   }
-} // setByMask
+/***/
 
 int ledMatHack (const LXI2CBusCtx *pC, const U8 busAddr)
 {
-static const U8 ctrlMask[]={   // byte-interleaved control matrices
-0b00000000, 0b10111111, 0b00111110, 0b00111110,
-0b00111111, 0b10111110, 0b00000111, 0b10000110,
-0b00110000, 0b00110000, 0b00111111, 0b10111110,
-0b00111111, 0b10111110, 0b01111111, 0b11111110,
-0b01111111, 0b00000000};
-/* Patterns ????
-0000000000        1 0 111111 00
-11111    000      11111 0
-111111   00000    1 0 11111 0
-111      00       1 0000 11 0
-11       000000   00 11 0000
-111111   00       1 0 11111 0
-111111   0        1 0 11111 0
-1111111  0        1111111 0,
-1111111           00000000
-*/
    //LOG_CALL("() - %d bytes, %d bits set\n", sizeof(b), bitCountNU8(b, sizeof(b)));
    int r;
    FramePage   frames[8];
    ControlPage cp;
-   U8 pageSel[2]={REG_PAGE_SEL,CTRL_PAGE};
+   U8 pageSel[2]={LMSL_REG_PAGE_SEL,LMSL_CTRL_PAGE};
 
-   LOG("ctrlMask[%d]=\n", FLAG_BYTES);
-   for (int i=0; i<FLAG_BYTES; i+= 2) LOG("%d : %02X %02X\n", i, ctrlMask[i], ctrlMask[i+1]);
    memset(frames, 0x00, sizeof(frames));
    // All pages start at 0x00
    //for (int i=0; i<8; i++) { frames[i].addr[0]= 0x00; }
@@ -68,10 +25,10 @@ static const U8 ctrlMask[]={   // byte-interleaved control matrices
    //memset(cp.reg, 0xFF, sizeof(cp.reg));
    //for (int i=0; i<13; i++) printf("%d : %02X\n", i, cp.reg[i]);
    r= lxi2cReadRB(pC, busAddr, pageSel, sizeof(pageSel));
-   if (CTRL_PAGE != pageSel[1])
+   if (LMSL_CTRL_PAGE != pageSel[1])
    {
       printf("got page %x\n", pageSel[1]);
-      pageSel[1]= CTRL_PAGE;
+      pageSel[1]= LMSL_CTRL_PAGE;
       r= lxi2cWriteRB(pC, busAddr, pageSel, sizeof(pageSel));
    }
 
@@ -81,19 +38,30 @@ static const U8 ctrlMask[]={   // byte-interleaved control matrices
    {
       for (int i=0; i<13; i++) LOG("%d : %02X\n", i, cp.reg[i]);
    }
-   pageSel[1]= 0x00; // Frame 0
+   pageSel[1]= LMSL_DATA_PAGE1; // Frame index 0
    r= lxi2cWriteRB(pC, busAddr, pageSel, sizeof(pageSel));
-   r= lxi2cReadRB(pC, busAddr, frames[0].addr, sizeof(FramePage));
-   LOG("Read Frame Page - r=%d\n", r);
+
+   if (0)
+   {
+      r= lxi2cReadRB(pC, busAddr, frames[0].addr, sizeof(FramePage));
+      LOG("Read Frame Page - r=%d\n", r);
+   }
+   else
+   {
+      r= ledMapSetBits(frames[0].enable, LMSL_FLAG_BYTES);
+      memset(frames[0].blink, 0x00, LMSL_FLAG_BYTES);
+      memset(frames[0].pwm, 0x00, LMSL_PWM_BYTES);
+   }
+   //for (int i=0; i<LMSL_FLAG_BYTES; i+= 2) LOG("%d : %02X %02X\n", i, frames[0].enable[i], frames[0].enable[i+1]);
+
    if (r >= 0)
    {
-      for (int i=0; i<FLAG_BYTES; i+= 2) LOG("%d : %02X %02X\n", i, frames[0].enable[i], frames[0].enable[i+1]);
-      r= 0;
-      memcpy(frames[0].enable, ctrlMask, FLAG_BYTES);
-      memset(frames[0].blink, 0x00, FLAG_BYTES);
-      memset(frames[0].pwm, 0x00, PWM_BYTES);
-      U8 v[2]= {0x00, 0x1F};
-      setByMask(frames[0].pwm, 40, ctrlMask, v); // PWM_BYTES
+      U8 v[]= {0x00, 0x1F, 0xFF, 0x1F};
+      ledMapChanPWM(frames[0].pwm, v, SHIM_LED_COUNT, 0x03, gMapLED.red); // red);
+      //ledMapChanPWM(frames[0].pwm, v, SHIM_LED_COUNT-5, 0x03, gMapLED.green+5); // red);
+      ledMapChanPWM(frames[0].pwm, v, SHIM_LED_COUNT, 0x03, gMapLED.blue); // red);
+      //ledMapIdxChanPWM(frames[0].pwm, v, 0x03, 0);
+
       r= lxi2cWriteRB(pC, busAddr, frames[0].addr, sizeof(FramePage));
       LOG("Write Frame Page - r=%d\n", r);
    }
--- MBD/ledMatrix.h
+++ MBD/ledMatrix.h
@@ -1,3 +1,7 @@
+// Common/MBD/ledMatrix.h - Hacks for IS31FL3731 LEDshim
+// https://github.com/DrAl-HFS/Common.git
+// Licence: GPL V3
+// (c) Project Contributors Oct. 2020
 
 #ifndef LED_MATRIX_H
 #define LED_MATRIX_H
@@ -6,6 +10,8 @@
 #include "mbdUtil.h"
 #include "lxI2C.h"
 
+
+/***/
 
 extern int ledMatHack (const LXI2CBusCtx *pC, const U8 busAddr);
 
