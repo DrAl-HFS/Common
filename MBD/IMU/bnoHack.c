8d986f715ab3ae4a4857639f120dc7701f45a8a6
DrAl-HFS <DrAl-HFS@github.com>
2021-02-15 17:04:03 +0000

    TLV tested

--- MBD/IMU/bnoHack.c
+++ MBD/IMU/bnoHack.c
@@ -9,7 +9,8 @@
 #include "lxTiming.h"
 
 #define BNO_PLD_MAX  ((1<<15)-5)
-#define BNO_PLD_STD  128
+#define BNO_PLD_INIT 508
+#define BNO_PLD_STD  60
 
 // "Sensor Hub Transport Protocol" - Hillcrest labs proprietary
 typedef union
@@ -40,15 +41,18 @@ typedef struct
    PldReqID pld;
 } PktReqID;
 
-typedef struct
-{
-   BNOHdr hdr;
-   U8 pld[BNO_PLD_STD];
-} PktAny;
+typedef struct { BNOHdr hdr; U8 pld[BNO_PLD_MAX]; } PktMax;
+typedef struct { BNOHdr hdr; U8 pld[BNO_PLD_INIT]; } PktInit;
+typedef struct { BNOHdr hdr; U8 pld[BNO_PLD_STD]; } PktStd;
+typedef struct { BNOHdr hdr; U8 pld[]; } PktAny; // NB : zero-sized array extension
+
+typedef struct { U8 t; union { int v; const char *p; }; } TLVDatum;
 
 #ifndef I2C_BLK_BYTES
 #define I2C_BLK_BYTES   (1<<9)
 #endif
+
+#define TLV_MAX 40
 
 /***/
 
@@ -71,6 +75,43 @@ static const U8 bytesFx[]=
    return(0);
 } // payloadBytes
 
+int addTLV (TLVDatum *pD, const U8 b[], const int remB)
+{
+   int s= 0;
+   if (remB > 2)
+   {
+      if (((b[0] > 0) && (b[0] < 10)) || (0x80 == b[0]))
+      {
+         s= 2 + b[1];
+         if (remB >= s)
+         {
+            pD->t= b[0];
+            if (b[1] > 4) { pD->t|= 0x80; }
+            if (0x80 & pD->t)
+            { // ASCII
+               int e= b[1]-1;
+               pD->p= (void*)(b+2);
+               if (!(printable(pD->p[0]) && (0 == pD->p[e]))) { pD->t|= 0x40; }
+            }
+            else { pD->v= readBytesLE(b, 2, b[1]); }
+         }
+         else { s= remB-s; } // shortfall
+      }
+   }
+   return(s);
+} // addTLV
+
+int scanTLV (TLVDatum d[], const int maxD, const U8 b[], const int maxB)
+{
+   int iB=0, nD=0;
+   while ((iB < maxB) && (nD < maxD))
+   {
+      int s= addTLV(d+nD, b+iB, maxB-iB);
+      if (s > 0) { iB+= s; nD++; } else { iB++; }
+   }
+   return(nD);
+} // scanTLV
+
 int validID (const PldID *pP) { return(0xF8 == pP->id); }
 
 int dumpID (const PldID *pP)
@@ -92,9 +133,9 @@ U16 dumpHdr (const BNOHdr *pH, int i)
    return(pld);
 } // dumpHdr
 
-int dumpRead (const LXI2CBusCtx *pC, const U8 busAddr, PktAny *pA)
+int dumpRead (const LXI2CBusCtx *pC, const U8 busAddr, PktAny *pA, const int max)
 {
-   const int max= MIN(sizeof(*pA), I2C_BLK_BYTES);
+   //const int max= MIN(sizeof(*pA), I2C_BLK_BYTES);
    int l=0, r=0;
    r= lxi2cReadStream(pC, busAddr, pA->hdr.b, sizeof(pA->hdr));
    if (r >= 0)
@@ -111,7 +152,7 @@ int dumpRead (const LXI2CBusCtx *pC, const U8 busAddr, PktAny *pA)
             {
                l= dumpHdr(&(pA->hdr),0);
                b= MIN(b, l);
-               reportBytes(0, pA->pld, b);
+               reportBytes(OUT, pA->pld, b);
                t+= b; i++;
             }
          } while ((r >= 0) && (t < s));
@@ -124,7 +165,7 @@ int dumpRead (const LXI2CBusCtx *pC, const U8 busAddr, PktAny *pA)
 
 int bnoReset (const LXI2CBusCtx *pC, const U8 busAddr, int syncDelay)
 {
-   PktAny any;
+   PktStd any;
    int r;
    any.hdr.len= 5;
    any.hdr.chan= 1;  // "exe"
@@ -135,14 +176,37 @@ int bnoReset (const LXI2CBusCtx *pC, const U8 busAddr, int syncDelay)
    return(r);
 } // bnoReset
 
+int init (const LXI2CBusCtx *pC, const U8 busAddr)
+{
+   TLVDatum tlv[TLV_MAX];
+   PktInit any;
+   int r;
+
+   bnoReset(pC, busAddr, 10);
+   r= dumpRead(pC, busAddr, (void*)&any, BNO_PLD_INIT+4); // flush any junk
+   if (r > 0)
+   {
+      int n= scanTLV(tlv, TLV_MAX, any.pld, r);
+      LOG_CALL("() - found %d TLV\n", n);
+      for (int i=0; i<n; i++)
+      {
+         U8 x= 0xC0 & tlv[i].t;
+         LOG("0x%X : ", tlv[i].t & 0x3F);
+         if (0 == x) { LOG("%d\n", tlv[i].v); }
+         else if (0x80 == x) { LOG("%s\n", tlv[i].p); }
+         else { LOG("0x%02X?\n", tlv[i].p[0]); }
+      }
+   }
+   return(r);
+} // init
+
 int bnoHack (const LXI2CBusCtx *pC, const U8 busAddr, const U8 modeFlags)
 {
    PktReqID pkt;
-   PktAny any;
+   PktStd any;
    int r=0, n=0, tries=3;
 
-   bnoReset(pC, busAddr, 10);
-   dumpRead(pC,busAddr,&any); // flush any junk
+   init(pC,busAddr);
 
    pkt.hdr.len= sizeof(pkt);
    if (6 != pkt.hdr.len) { WARN_CALL("() - align pad! (%d)\n", pkt.hdr.len); pkt.hdr.len= 6; }
@@ -158,7 +222,7 @@ int bnoHack (const LXI2CBusCtx *pC, const U8 busAddr, const U8 modeFlags)
       {
          dumpHdr(&(pkt.hdr),1);
          pkt.hdr.seq++;
-         int i=0, s=1, l= dumpRead(pC,busAddr,&any);
+         int i=0, s=1, l= dumpRead(pC, busAddr, (void*)&any, BNO_PLD_STD+4);
          while ((i < l) && (s > 0))
          {
             s= dumpID((void*)(any.pld+i));
